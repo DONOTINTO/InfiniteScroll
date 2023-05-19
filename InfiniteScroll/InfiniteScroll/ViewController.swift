7f6080e8f022cb92af865f8757c13d5274428096
DONOTINTO <goods99j@gmail.com>
2023-05-19 20:02:22 +0900

    [add] 프로젝트 생성

---
+++ InfiniteScroll/InfiniteScroll/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InfiniteScroll
+//
+//  Created by 이중엽 on 2023/05/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
