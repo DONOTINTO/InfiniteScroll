d754cb2beebe1ec73e8d75191687e95c8319713f
DONOTINTO <goods99j@gmail.com>
2023-05-24 22:54:18 +0900

    [update] 데이터 Decode

--- InfiniteScroll/InfiniteScroll/StoryViewController.swift
+++ InfiniteScroll/InfiniteScroll/StoryViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class StoryViewController: UIViewController {
     @IBOutlet weak var storyTableView: UITableView!
-    let story = "The prince befriends a fox, who teaches him that the important things in life are visible only to the heart, that his time away from the rose makes the rose more special to him, and that love makes a person responsible for the beings that one loves. The little prince realizes that, even though there are many roses, his love for his rose makes her unique and that he is therefore responsible for her. Despite this revelation, he still feels very lonely because he is so far away from his rose. The prince ends his story by describing his encounters with two men, a railway switchman and a salesclerk."
+    var story = "The prince befriends a fox, who teaches him that the important things in life are visible only to the heart, that his time away from the rose makes the rose more special to him, and that love makes a person responsible for the beings that one loves. The little prince realizes that, even though there are many roses, his love for his rose makes her unique and that he is therefore responsible for her. Despite this revelation, he still feels very lonely because he is so far away from his rose. The prince ends his story by describing his encounters with two men, a railway switchman and a salesclerk."
     var storyList: [String] = []
     var actualList: [String] = []
     var perPage = 10
@@ -19,8 +19,6 @@ class StoryViewController: UIViewController {
         storyTableView.delegate = self
         storyTableView.dataSource = self
         storyTableView.prefetchDataSource = self
-        storyList = story.components(separatedBy: " ")
-        actualList.append(contentsOf: storyList[0...10])
         getBlogInfo(query: "123")
     }
     
@@ -45,18 +43,34 @@ class StoryViewController: UIViewController {
         
         var url = URL(string: baseURL)
         url?.append(queryItems: [
-            URLQueryItem(name: "query", value: query)
+            URLQueryItem(name: "query", value: query),
+            URLQueryItem(name: "size", value: "1")
         ])
         
         guard let url = url else { return }
         
         var request = URLRequest(url: url)
         request.setValue(Authorization, forHTTPHeaderField: "Authorization")
+        
         URLSession.shared.dataTask(with: request) { data, response, error in
             guard let response = response as? HTTPURLResponse else { return }
             guard error == nil else { return }
             guard let data = data else { return }
             
+            do {
+                let decoder = JSONDecoder()
+                let kakao = try decoder.decode(Kakao.self, from: data)
+                let kakaoData = kakao.documents
+                
+                DispatchQueue.main.sync {
+                    self.story = kakaoData[0].contents
+                    self.storyList = self.story.components(separatedBy: " ")
+                    self.actualList.append(contentsOf: self.storyList[0...10])
+                    self.storyTableView.reloadData()
+                }
+            } catch {
+                print(error.localizedDescription)
+            }
             print(response.statusCode)
         }.resume()
     }
